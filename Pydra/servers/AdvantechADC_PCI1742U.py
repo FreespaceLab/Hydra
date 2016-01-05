3049b15cfb73853719a4339c8900a2c47bfffe2d
FreespaceLab <410374387@qq.com>
2016-01-05 16:06:54 +0800

    modified the use of jpype, into a single thread mode
    which is not the final vesion: the code orgnization is in a test mode

--- Pydra/servers/AdvantechADC_PCI1742U.py
+++ Pydra/servers/AdvantechADC_PCI1742U.py
@@ -2,30 +2,52 @@ __author__ = 'Hwaipy'
 import sys
 import jpype
 from InstrumentServers import InstrumentServer
+import queue
 
 class AdvantechADC_PCI1742U:
     def __init__(self):
-        jpype.startJVM(jpype.getDefaultJVMPath(),'-Djava.class.path=..\\..\\Jydra\\AdvantechADCPCI\\build\\classes\\;..\\..\\Jydra\\AdvantechADCPCI\\lib\\jna-3.0.9.jar')
-        deviceManager = jpype.JClass('com.hwaipy.unifieddeviceinterface.adc.advantech.AdvantechADCDeviceManager').getManager()
-        self.jpypeObject = deviceManager.getDeviceList()[0]
-        self.jpypeObject.open()
+        self.queue = queue.Queue()
+        def controlJPype():
+            jpype.startJVM(jpype.getDefaultJVMPath(),'-Djava.class.path=..\\Jydra\\AdvantechADCPCI\\build\\classes\\;..\\Jydra\\AdvantechADCPCI\\lib\\jna-3.0.9.jar')
+            deviceManager = jpype.JClass('com.hwaipy.unifieddeviceinterface.adc.advantech.AdvantechADCDeviceManager').getManager()
+            jpypeObject = deviceManager.getDeviceList()[0]
+            jpypeObject.open()
+            while True:
+                action = self.queue.get()
+                action(jpypeObject)
+        threading._start_new_thread(controlJPype,())
 
     def readAnalogVoltage(self,channel):
         if channel>=0 and channel<16:
-            return self.jpypeObject.analogIOReadVoltage(channel)
+            try:
+                semaphore = threading.Semaphore(0)
+                returnValue = []
+                def action(jpypeObject):
+                    returnValue.append(jpypeObject.analogIOReadVoltage(channel))
+                    semaphore.release()
+                self.queue.put(action)
+                semaphore.acquire()
+                return returnValue[0]
+            except Exception as e:
+                print(e)
         else:
             raise
 
 def runAsService():
-    jpype.startJVM(jpype.getDefaultJVMPath(),'-Djava.class.path=E:\\Coding\\Examples\\Programs\\CODE\\AdvantechADCPCI\\build\\classes\\;E:\\Coding\\Examples\\Programs\\CODE\\Lib\\Native\\jna-3.0.9.jar')
-    deviceManager = jpype.JClass('com.hwaipy.unifieddeviceinterface.adc.advantech.AdvantechADCDeviceManager').getManager()
-    device0 = deviceManager.getDeviceList()[0]
-    device0.open()
-    device = AdvantechADC_PCI1742U(device0)
+    #jpype.startJVM(jpype.getDefaultJVMPath(),'-Djava.class.path=E:\\Coding\\Examples\\Programs\\CODE\\AdvantechADCPCI\\build\\classes\\;E:\\Coding\\Examples\\Programs\\CODE\\Lib\\Native\\jna-3.0.9.jar')
+    #deviceManager = jpype.JClass('com.hwaipy.unifieddeviceinterface.adc.advantech.AdvantechADCDeviceManager').getManager()
+    #device0 = deviceManager.getDeviceList()[0]
+    #device0.open()
+    device = AdvantechADC_PCI1742U()
     server = InstrumentServer('R22_AdvantechADC_PCI1742U',address=('172.16.60.199',20102),invokers=[device])
     server.start()
 
 if __name__ == '__main__':
+    import threading
+    import time
+    threading._start_new_thread(runAsService(), ())
+    time.sleep(1000)
+
     runAsService()
     try:
         jpype.startJVM(jpype.getDefaultJVMPath(),
